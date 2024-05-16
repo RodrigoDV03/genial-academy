da908bdb0979d6960a85e008a52c58b52298d430
RodrigoDV03 <rodrigo.davila1@unmsm.edu.pe>
2024-05-16 09:06:35 -0500

    Se implementa ventana modal de estructura de examen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ModalExam } from '../ModalExam'
 import { Link } from 'react-router-dom'
+import './styles.css'
+
 
 export const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+
     return (
         <div className="container">
             <h1>Home</h1>
@@ -17,6 +22,17 @@ export const Home = () => {
                     </Link>
                 </li>
             </ul>
+
+            <div className="buttonContainer">
+                <button onClick={() => setShowModal(!showModal)}>Ventana modal</button>
+            </div>
+            
+            <ModalExam estado = {showModal} setEstado = {setShowModal}>
+                <div className="Contenido">
+                    <h1>Estructura de examen de admisión por área</h1>
+                    <div className="img"></div>
+                </div>
+            </ModalExam>
         </div>
     )
 }
