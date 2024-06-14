7dfe4c0d9a0e6532674127817585f999ff639db1
JoshuaPariona <joshiparsa@gmail.com>
2024-06-14 14:26:25 -0500

    se modifico el faq y se agrego imagen

--- src/pages/Home/components/Faq/index.jsx
+++ src/pages/Home/components/Faq/index.jsx
@@ -3,110 +3,76 @@ import "./styles.css";
 
 
 export const FaqSection = () => {
-    const [expandCollapseState,setExpandCollapseState] = useState([true, true, true, true])
+    const [expandCollapseState,setExpandCollapseState] = useState([false, false, false, false])
 
     const expandCollapse = (index) => {
         const newExpandCollapseState = [...expandCollapseState];
         newExpandCollapseState[index] = !newExpandCollapseState[index];
         setExpandCollapseState(newExpandCollapseState)
     }
+  
+    const FAQ = [
+      {
+        question: "¿Cómo me registro en la plataforma?",
+        answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        sint error accusantium ab, atque quidem illum illo, dolorum
+        cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
+        eveniet nobis necessitatibus sapiente!`
+      },
+      {
+        question: "¿Qué planes tiene la plataforma?",
+        answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        sint error accusantium ab, atque quidem illum illo, dolorum
+        cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
+        eveniet nobis necessitatibus sapiente!`
+      },
+      {
+        question: "¿Qué funcionalidades ofrece?",
+        answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        sint error accusantium ab, atque quidem illum illo, dolorum
+        cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
+        eveniet nobis necessitatibus sapiente!`
+      },
+      {
+        question: "¿Obtengo algun premio si gano puntos?",
+        answer: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        sint error accusantium ab, atque quidem illum illo, dolorum
+        cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
+        eveniet nobis necessitatibus sapiente!`
+      },
+    ]
+
+    function faqTile(faq, index) {
+      return (
+        <div className="faq_tile">
+          <div className="faq_tile_collapse">
+            <div className="faq_tile_title">
+              <p>{faq.question}</p>
+            </div>
+            <div className="faq_tile_trail" onClick={() => expandCollapse(index)}>
+              <img src={expandCollapseState[index]? "src/assets/images/arrow_collapse.png": "src/assets/images/arrow_expanded.png"} alt="" />
+            </div>
+          </div>
+          <div className="faq_tile_expanded">
+            <p>{expandCollapseState[index]? faq.answer: ""}</p>
+          </div>
+        </div>
+      );
+    }
+
+    function generateTiles() {
+      const html = [];
+      FAQ.forEach((faq, index) => {
+        html.push(faqTile(faq, index))
+      });
+      return html;
+    }
 
   return (
     <div className="faq_screen">
       <div className="faq_title">Preguntas Frecuentes</div>
       <div className="faq_content">
-        <div className="faq_tile" onClick={() => expandCollapse(0)}>
-          <div className="faq_tile_collapse">
-            <div className="faq_tile_title">
-              <p>¿Cómo me registro en la plataforma?</p>
-            </div>
-            <div className="faq_tile_trail"></div>
-          </div>
-          <div
-            className={
-              expandCollapseState[0]
-                ? "faq_tile_expanded hidden"
-                : "faq_tile_expanded"
-            }
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              sint error accusantium ab, atque quidem illum illo, dolorum
-              cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
-              eveniet nobis necessitatibus sapiente!
-            </p>
-          </div>
-        </div>
-
-        <div className="faq_tile" onClick={() => expandCollapse(1)}>
-          <div className="faq_tile_collapse">
-            <div className="faq_tile_title">
-              <p>¿Qué planes tiene la plataforma?</p>
-            </div>
-            <div className="faq_tile_trail"></div>
-          </div>
-          <div
-            className={
-              expandCollapseState[1]
-                ? "faq_tile_expanded hidden"
-                : "faq_tile_expanded"
-            }
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              sint error accusantium ab, atque quidem illum illo, dolorum
-              cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
-              eveniet nobis necessitatibus sapiente!
-            </p>
-          </div>
-        </div>
-
-        <div className="faq_tile" onClick={() => expandCollapse(2)}>
-          <div className="faq_tile_collapse">
-            <div className="faq_tile_title">
-              <p>¿Qué funcionalidades ofrece?</p>
-            </div>
-            <div className="faq_tile_trail"></div>
-          </div>
-          <div
-            className={
-              expandCollapseState[2]
-                ? "faq_tile_expanded hidden"
-                : "faq_tile_expanded"
-            }
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              sint error accusantium ab, atque quidem illum illo, dolorum
-              cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
-              eveniet nobis necessitatibus sapiente!
-            </p>
-          </div>
-        </div>
-        
-        <div className="faq_tile" onClick={() => expandCollapse(3)}>
-          <div className="faq_tile_collapse">
-            <div className="faq_tile_title">
-              <p>¿Obtengo algun premio si gano puntos?</p>
-            </div>
-            <div className="faq_tile_trail"></div>
-          </div>
-          <div
-            className={
-              expandCollapseState[3]
-                ? "faq_tile_expanded hidden"
-                : "faq_tile_expanded"
-            }
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              sint error accusantium ab, atque quidem illum illo, dolorum
-              cupiditate dolorem voluptatum quasi. Maxime vel obcaecati autem
-              eveniet nobis necessitatibus sapiente!
-            </p>
-          </div>
-        </div>
-         
+        {generateTiles()}
       </div>
     </div>
   );
